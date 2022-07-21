4afb5e6b0893e308fbe214e314421433dd85e95f
JusKalMeDZ <juskalmedz@gmail.com>
2022-07-21 19:58:06 +0700

    feat: query params

--- src/containers/MovieList.js
+++ src/containers/MovieList.js
@@ -1,40 +1,61 @@
-import { Box } from '@mui/material';
-import { useEffect, useState } from 'react';
+import { Box, Button } from "@mui/material";
+import { useEffect, useState } from "react";
 
-import tmdb from '../apis/tmdb';
-import MovieCard from '../components/MovieCard';
+import tmdb from "../apis/tmdb";
+import MovieCard from "../components/MovieCard";
+import { useSearchParams } from "react-router-dom";
 
 const MovieList = () => {
-    const [movies, setMovies] = useState([]);
+  const [queryParams, setQueryPrams] = useSearchParams();
+  const [movies, setMovies] = useState([]);
 
-    useEffect(() => {
-        const fetchMovies = async () => {
-            try {
-                const fetchedMovies = await tmdb.get("trending/movie/week");
-                setMovies(fetchedMovies.data.results);
-            } catch (error) {
-                console.log(error);
-            }
-        }
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const fetchedMovies = await tmdb.get("trending/movie/week");
+        setMovies(fetchedMovies.data.results);
+      } catch (error) {
+        console.log(error);
+      }
+    };
 
-        fetchMovies();
-    }, []);
+    fetchMovies();
+  }, []);
 
-    return (
-        <Box sx={{
-            display: 'flex',
-            flexDirection: 'row',
-            flexWrap: 'wrap',
-            justifyContent: 'space-between',
-            mt: 5,
-        }}>
-            {
-                movies.map(movie => (
-                    <MovieCard movie={movie}></MovieCard>
-                ))
-            }
-        </Box>
-    );
-}
+  const setSortParams = (type) => {
+    queryParams.set("sort", type);
+    setQueryPrams(queryParams);
+  };
+  return (
+    <>
+      <Box
+        sx={{
+          mt: 10,
+          display: "flex",
+          flexDirection: "row",
+          justifyContent: "flex-end",
+          alignItems: "center",
+        }}
+      >
+        Sort by rating
+        <Button onClick={() => setSortParams("asc")}>Asc</Button>
+        <Button onClick={() => setSortParams("desc")}>Desc</Button>
+      </Box>
+      <Box
+        sx={{
+          display: "flex",
+          flexDirection: "row",
+          flexWrap: "wrap",
+          justifyContent: "space-between",
+          mt: 5,
+        }}
+      >
+        {movies.map((movie) => (
+          <MovieCard movie={movie}></MovieCard>
+        ))}
+      </Box>
+    </>
+  );
+};
 
 export default MovieList;

33f90d502804bdb40bca014442f65eccfe33db31
JusKalMeDZ <juskalmedz@gmail.com>
2022-07-21 19:08:29 +0700

    feat: test

--- src/apis/tmdb.js
+++ src/apis/tmdb.js
@@ -1,7 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
 
-const API_KEY = '';
-const baseUrl = 'https://api.themoviedb.org/3/';
+const API_KEY = "70e79ce04a3ea177fe15eedfa5c4d6f0";
+const baseUrl = "https://api.themoviedb.org/3/";
 
 const tmdb = axios.create({
   baseURL: baseUrl,

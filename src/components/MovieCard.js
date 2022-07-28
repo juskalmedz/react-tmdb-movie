56d679fe4a7e4bacbdc6a937bee3743dd479d014
JusKalMeDZ <juskalmedz@gmail.com>
2022-07-28 23:07:13 +0700

    feat: with axios

--- src/components/MovieCard.js
+++ src/components/MovieCard.js
@@ -1,41 +1,52 @@
-import { Box, CardMedia, Rating } from '@mui/material';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
-import * as React from 'react';
+import { Box, CardMedia, Rating } from "@mui/material";
+import Card from "@mui/material/Card";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import * as React from "react";
+
+const BASE_IMAGE_URL = "https://image.tmdb.org/t/p/original";
 
 const MovieCard = ({ movie }) => {
   return (
-    <Card id={movie.id} sx={{ display: 'flex', width: 400, margin: 5 }}>
+    <Card id={movie.id} sx={{ display: "flex", width: 400, margin: 5 }}>
       <CardMedia
         component="img"
         sx={{ width: 150, height: 225 }}
-        image="http://placekitten.com/500"
+        image={BASE_IMAGE_URL + movie.poster_path}
         alt="Live from space album cover"
       />
-      <Box sx={{ display: 'flex', flexDirection: 'column' }}>
-        <CardContent sx={{ flex: '1 0 auto' }}>
+      <Box sx={{ display: "flex", flexDirection: "column" }}>
+        <CardContent sx={{ flex: "1 0 auto" }}>
           <Typography component="div" variant="h6">
             {movie.title}
           </Typography>
-          <Typography variant="subtitle1" color="text.secondary" component="div">
+          <Typography
+            variant="subtitle1"
+            color="text.secondary"
+            component="div"
+          >
             {new Date(movie.release_date).getFullYear()}
           </Typography>
           <Box
             sx={{
               width: 200,
-              display: 'flex',
-              alignItems: 'center',
+              display: "flex",
+              alignItems: "center",
             }}
           >
-            <Rating name="read-only" precision={0.1} value={movie.vote_average / 2} max={5} readOnly />
+            <Rating
+              name="read-only"
+              precision={0.1}
+              value={movie.vote_average / 2}
+              max={5}
+              readOnly
+            />
             <Box sx={{ ml: 2 }}>{movie.vote_average}</Box>
           </Box>
-
         </CardContent>
       </Box>
     </Card>
   );
-}
+};
 
 export default MovieCard;
--- src/containers/MovieList.js
+++ src/containers/MovieList.js
@@ -1,27 +1,38 @@
-import { Box } from '@mui/material';
-import { useState } from 'react';
+import { Box } from "@mui/material";
+import { useEffect, useState } from "react";
+import axios from "axios";
 
-import MovieCard from '../components/MovieCard';
-import fetchedMovies from '../data/fetchedMovies.json';
+import MovieCard from "../components/MovieCard";
+import fetchedMovies from "../data/fetchedMovies.json";
 
 const MovieList = () => {
-    const [movies, setMovies] = useState(fetchedMovies.results);
+  const [movies, setMovies] = useState([]);
+  const URL =
+    "https://api.themoviedb.org/3/trending/movie/week?api_key=70e79ce04a3ea177fe15eedfa5c4d6f0";
+  const fetchData = async () => {
+    const response = await axios.get(URL);
+    setMovies(response.data.results);
+  };
 
-    return (
-        <Box sx={{
-            display: 'flex',
-            flexDirection: 'row',
-            flexWrap: 'wrap',
-            justifyContent: 'space-between',
-            mt: 5,
-        }}>
-            {
-                movies.map(movie => (
-                    <MovieCard movie={movie}></MovieCard>
-                ))
-            }
-        </Box>
-    );
-}
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        flexDirection: "row",
+        flexWrap: "wrap",
+        justifyContent: "space-between",
+        mt: 5,
+      }}
+    >
+      {movies.map((movie) => (
+        <MovieCard movie={movie}></MovieCard>
+      ))}
+    </Box>
+  );
+};
 
 export default MovieList;
